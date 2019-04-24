8381dae1fe3fa0f406abdc206f5a073eac373dff
Andrew Lee <andrew1421lee@gmail.com>
2019-04-24 19:45:42 -0400

    layouts all sketched out, with animations moving between

--- app/src/main/java/com/etirps/zhu/gachasimulator/MainActivity.kt
+++ app/src/main/java/com/etirps/zhu/gachasimulator/MainActivity.kt
@@ -3,22 +3,43 @@ package com.etirps.zhu.gachasimulator
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        // Get the index of what is currently displayed, 0 = game, 1 = collection, 3 = shop
+        val displayedView = content_flipper.displayedChild
+
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_game -> {
+                // Animate based on position
+                if(displayedView != 0) {
+                    content_flipper.inAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.in_from_left)
+                    content_flipper.outAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.out_from_right)
+                    content_flipper.displayedChild = 0
+                }
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_collection)
+            R.id.navigation_collection -> {
+                if(displayedView == 0) {
+                    content_flipper.inAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.in_from_right)
+                    content_flipper.outAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.out_from_left)
+                    content_flipper.displayedChild = 1
+                } else if(displayedView == 2) {
+                    content_flipper.inAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.in_from_left)
+                    content_flipper.outAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.out_from_right)
+                    content_flipper.displayedChild = 1
+                }
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_shop)
+            R.id.navigation_shop -> {
+                if(displayedView != 0) {
+                    content_flipper.inAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.in_from_right)
+                    content_flipper.outAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.out_from_left)
+                    content_flipper.displayedChild = 2
+                }
                 return@OnNavigationItemSelectedListener true
             }
         }
