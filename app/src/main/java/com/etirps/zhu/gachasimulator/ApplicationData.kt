a1e0676b335df0557bd78225e22d59772396731b
Andrew Lee <andrew1421lee@gmail.com>
2019-04-24 20:21:30 -0400

    framework for global variables added!

---
+++ app/src/main/java/com/etirps/zhu/gachasimulator/ApplicationData.kt
@@ -0,0 +1,7 @@
+package com.etirps.zhu.gachasimulator
+
+import android.app.Application
+
+class ApplicationData : Application() {
+    var cashMoney: Int = 100
+}
--- app/src/main/java/com/etirps/zhu/gachasimulator/GameController.kt
+++ app/src/main/java/com/etirps/zhu/gachasimulator/GameController.kt
@@ -2,12 +2,14 @@ package com.etirps.zhu.gachasimulator
 
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class GameController(private val game_layout: View) {
 
     private val onButtonClick = View.OnClickListener { view ->
         Toast.makeText(game_layout.context, "button clicked!!!", Toast.LENGTH_SHORT).show()
+        game_layout.findViewById<TextView>(R.id.message).text = (game_layout.context.applicationContext as ApplicationData).cashMoney.toString()
     }
 
     init {
